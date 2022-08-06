fc064dad1e1157605eafc6f8a7fa3d385b42cfa9
gulnisaslan <gulnisaslan@gmail.com>
2022-08-07 01:06:20 +0300

    First Commit

---
+++ First-week-assigement/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+
+        Scanner scanner = new Scanner(System.in);
+        int i,k,n,j,l,m;
+        System.out.println("Sayi giriniz");
+        n=scanner.nextInt();
+        for (i=1;i<=n;i++)
+                    {
+           for (k=n;k>i;k--)
+                    {
+                System.out.print(" ");
+                        }
+        for (j = 1; j <=i ; j++)//bu for döngümüzde ise yıldız şeklimizi çıkartıyor ve yıldızların arasındaki boşluğu ayarlıyor
+                        {
+                System.out.print("*"+" ");
+                        }
+                System.out.println();
+                    }
+        //bu döngümüz ile baklava diliminin alt kkısımını yapıyoruz
+        for (k=1;k<=n;k++)
+                    {
+        for (l =1 ; l <=k; l++)//döngümüz ile baklava diliminin alt kısımının sol tarafındaki boşlukları belirtiyoruz. 
+                        {
+                System.out.print(" ");
+                        }
+        for (m = n; m >k ; m--)//bu döngümüz ile baklava diliminin alt kısımındaki yıldızları ve arasındaki boşlukları belirtiyoruz.
+                        {
+                System.out.print("*"+" ");
+                        }
+                System.out.println();
+                    }
+
+    }
+
+    
+}
+
---
+++ readMe.md
@@ -0,0 +1,10 @@
+# Kodluyoruz 141.Java Bootcamp
+[Bootcamp Lesson Notes](Kodluyoruz-141.Izmir-Java-Bootcamp-Lesson-Notes) [via Huseyin Karabakla](https://github.com/hkarabakla/week-1)
+
+[First Week Asigement](\First-week-assigement)
+
+
+[Intstructor: Huseyin Karabakla](https://github.com/hkarabakla)
+
+
+
